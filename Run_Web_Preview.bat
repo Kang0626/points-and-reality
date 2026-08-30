a892049350dbaa7dfd2b60a144094f63d9973c22
Points & Reality <contact@pointsandreality.com>
2026-08-30 15:24:50 +1200

    feat(web): add ultra-smooth Canvas sequence scroll scrubber, 3DGS resource optimization, and typography word-wrap fixes

--- Run_Web_Preview.bat
+++ Run_Web_Preview.bat
@@ -1,10 +1,11 @@
 @echo off
-:: Points & Reality 3D Web Showroom Local Preview Launcher
-title Points & Reality Web Showroom
+:: Points & Reality 3D Web Studio Local Preview Launcher
+title Points & Reality Web Studio
 cd /d "%~dp0"
 
-echo [INFO] Starting local Web Showroom on http://127.0.0.1:8080 ...
-start "" http://127.0.0.1:8080/showroom.html
+echo [INFO] Starting local Web Studio on http://127.0.0.1:8080 ...
+start "" http://127.0.0.1:8080/index.html
 python -m http.server 8080
 
 pause
+
